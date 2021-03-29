86cdf9534d71b676e4de9988af9db5bbb325b1ee
NathanLoPresto <78660740+NathanLoPresto@users.noreply.github.com>
2021-03-29 14:20:59 -0500

    Update and rename Graphdata.py to graphadc.py

--- Python/graphadc.py
+++ Python/graphadc.py
@@ -1,58 +1,43 @@
 #Imports for time and matplotlib
+import random
 import time
 from matplotlib import pyplot as plt
 import matplotlib.animation as animation
 
-#opening and enumerating the opalkelly device
-dev = ok.okCFrontPanel()
-dev.OpenBySerial("")
-dev.ConfigureFPGA("example.bit")
-
-#addresses and masks for pipes and triggers
-pipeaddress= 0xa0
-trigaddress = 0x71
-trigmask = 0x80
-
-#Time between each trigger check and append
-terval=500
-
 #Initializing the start time of the program
 start_time= time.time()
-
-#checking for trigger, returns boolean 
-def trig():
-    dev.UpdateTriggerOuts()
-    if dev.IsTriggered(trigaddress, trigmask):
-        return True
-    else:
-        return False
-    
+#Time between each trigger check and append
+terval=200
 # Create figure for plotting
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 xs = []
 ys = []
 
-#creates a bytearray for pipeout
-datain = bytearray('00000000000000000000000000')
+def trig():
+    #trigger address and mask
+    trig_addr = 0x60
+    trig_mask = 0x01
 
+    fpga.xem.UpdateTriggerOuts()
+    if (fpga.xem.IsTriggered(trig_addr, trig_mask)):
+        return True
+    else:
+        return False
+        
 #Functions assigning values from pipeout to y, and time elapsed to x
 def y():
-   return dev.ReadFromPipeOut(pipeaddress, datain)
+   return random.randint(0,10)
 
 def x():
     return float((int((time.time()-start_time)*10))/10)
 
 # This function is called periodically from FuncAnimation
 def animate(i, xs, ys):
-    #if triggered, append x and y values
+    
     if trig():
         xs.append(x())
         ys.append(y())
-    
-    #printing x and y array values
-        print("The x values are: ", xs)
-        print("The y values are: ", ys)
     
     # Limits the x and y lists to 50 variables 
     xs = xs[-50:]
@@ -69,6 +54,8 @@ def animate(i, xs, ys):
     plt.ylabel('Value')
     plt.xlabel('Time (seconds since start)')
 
-# Set up plot to call animate() function periodically
+
 ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=terval)
 plt.show()
+
+
