23dec5a2c8c7977567daf6d0f0b544e5ca682799
NathanLoPresto <78660740+NathanLoPresto@users.noreply.github.com>
2021-04-14 18:47:11 -0500

    Update graphadc.py

--- Python/graphadc.py
+++ Python/graphadc.py
@@ -5,14 +5,14 @@ Created on Fri Apr  2 19:11:58 2021
 @author: Nathan LoPresto
 """
 
-#Imports for time and matplotlib
+#Imports for time, fpga, and matplotlib
 import time
 from fpga import FPGA
 from matplotlib import pyplot as plt
 
 #Local imports, needs the register map for the IC locations, 
-#AD7960 for plotting
-from register_map import ad5453
+#AD7960 for plotting the ADC data
+from register_map import ad5453, ad7952
 from AD7960driver import y
 
 #Initializing the start time of the program
@@ -23,6 +23,8 @@ fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 xs = []
 ys = []
+
+#instantiates f as the name of the FPGA being used. 
 f=FPGA()
 
 #Checks trigger, returns true if ready
@@ -33,7 +35,7 @@ def trig(trig_addr, trig_mask):
         return True
     else:
         return False
-        
+     
 #Function assigning the difference between start time and time elapsed
 def x():
     return float((int((time.time()-start_time)*10))/10)
@@ -42,7 +44,7 @@ def x():
 # This function is called periodically from FuncAnimation
 def animate(i, xs, ys):
     
-    if trig(ad5453['tx_register'], ad5453['control_register']):
+    if trig(ad5453['tx_register'], ad7952['control_register']):
         xs.append(x())
         ys.append(y())
     
@@ -57,7 +59,6 @@ def animate(i, xs, ys):
     # Format plot
     plt.xticks(rotation=45, ha='right')
     plt.subplots_adjust(bottom=0.30)
-    plt.title('The effect of time on value')
-    plt.ylabel('Value')
+    plt.title('The effect of time ADC output')
+    plt.ylabel('ADC output')
     plt.xlabel('Time (seconds since start)')
-
