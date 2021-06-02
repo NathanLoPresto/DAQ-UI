589923b8680048fa631581ae486ddd133e428440
NathanLoPresto <78660740+NathanLoPresto@users.noreply.github.com>
2021-06-02 14:22:25 -0500

    Update pyqtgraphing.py

--- Python/pyqtgraphing.py
+++ Python/pyqtgraphing.py
@@ -3,13 +3,16 @@ from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
 import sys  # We need sys so that we can pass argv to QApplication
 import os
+import ok
 from random import randint
 import time
+from fpga import FPGA
 from AD7960driver import y 
+from CSVfilemaker import filemaker
 
 #Initializing the start time of the program
 start_time= time.time()
-
+d1 = []
 
 #Function assigning the difference between start time and time elapsed
 def x():
@@ -42,12 +45,20 @@ class MainWindow(QtWidgets.QMainWindow):
         self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
 
         self.y = self.y[1:]  # Remove the first
-        self.y.append( y())  # Add a new random value.
+        b=y()
+        self.y.append(b)  # Add a new random value.
+        d1.append(b) #appending to the evential CSV file
 
         self.data_line.setData(self.x, self.y)  # Update the data.
 
+xem = ok.okCFrontPanel()
+if (xem.NoError != xem.OpenBySerial("")):
+    print ("The graphing will not commence due to an error")
 
-app = QtWidgets.QApplication(sys.argv)
-w = MainWindow()
-w.show()
-sys.exit(app.exec_())
+else:
+    app = QtWidgets.QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec_())
+
+filemaker(d1)
