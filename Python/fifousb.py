1eeaa7c363a75a85f514c61df611cd3ae487a847
NathanLoPresto <78660740+NathanLoPresto@users.noreply.github.com>
2021-06-02 14:21:55 -0500

    Update fifousb.py

--- Python/fifousb.py
+++ Python/fifousb.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  1 11:55:57 2021
+
+@author: Nathan LoPresto
+"""
+
 #in the end, I intend for this class to be drawn by ad7960driver
 import ok
 from fpga import FPGA
@@ -21,7 +28,7 @@ adc_pipe_rd_trig = ep(0x70, [3,4,5,6], 'to')
 
 def fifo_read(): 
     # Send brief reset signal to initialize the FIFO
-    buf = dev.read_pipe_out(adc_pipe)
+    buf,e  = f.read_pipe_out(0xA1)
     return buf
     dev.reset_device()
 
