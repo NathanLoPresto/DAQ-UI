87572c98693289c46fe12a6c5b7d144561de4ca5
NathanLoPresto <nlopresto14@gmail.com>
2021-07-13 15:00:12 -0500

    usable

--- DDR3/DDR.py
+++ DDR3/DDR.py
@@ -38,8 +38,6 @@ def make_sin_wave(amplitude_shift, frequency_shift=16):
     frequency_shift = frequency_shift/np.pi/2
     time_axis = np.arange (0, np.pi*2 , (1/1000000*2/np.pi) )
     amplitude = (amplitude_shift*1000*np.sin(time_axis))
-    for x in range (len(amplitude)):
-        amplitude[x] = (int)(amplitude[x])
     amplitude = amplitude.astype(np.int32)
     return time_axis, amplitude
 
@@ -56,9 +54,7 @@ def writeSDRAM(g_buf):
     #Enable SDRAM write memory transfers
     f.xem.SetWireInValue(0x00, 0x0002)
     f.xem.UpdateWireIns()
-
     print ("Writing to DDR...")
-
     for i in range ((int)(g_nMemSize/WRITE_SIZE)):
         r = f.xem.WriteToBlockPipeIn( epAddr= 0x80, blockSize= BLOCK_SIZE,
                                       data= g_buf[(WRITE_SIZE*i):((WRITE_SIZE*i)+WRITE_SIZE)])
@@ -87,11 +83,12 @@ def readSDRAM(g_rbuf):
     change_read = (end_read - start_read)
     read_speed = (1/change_read)
     print ("The speed of the read was ", (int)(read_speed), " MegaBytes per second")
+    return g_rbuf
 
 #given a buffer, it unpacks into into human readable float values
 def unpack(buf):
     unpacked_var = []
-    for x in range (BLOCK_SIZE):
+    for x in range (1000000):
         unpacked_var.append(struct.unpack('i', buf[(x*4):((x+1)*4)]))
     return unpacked_var
 
@@ -110,9 +107,7 @@ def write_sin_wave (a):
     #g_buf = bytearray(np.asarray(np.ones(1000000), np.uint8))
     time_axis, g_buf_init = make_sin_wave(a)
     testplot(time_axis, g_buf_init)
-    #g_buf = bytearray(g_buf_init)
     g_buf = bytearray(g_buf_init)
-    print (len(g_buf))
     writeSDRAM(g_buf)
 
 #given and amplitude and a period, it will write a step function to the DDR3 
@@ -125,21 +120,18 @@ def write_step_func(a,b):
 #Reads and prints the contents of the DDR3
 def print_DDR3():
     g_rbuf = bytearray(np.asarray(np.zeros(READBUF_SIZE), np.uint8))
-    readSDRAM(g_rbuf)
+    g_rbuf = readSDRAM(g_rbuf)
     unpacked_g_rbuf = np.array(unpack(g_rbuf)).astype('float64')
     for x in range (len(unpacked_g_rbuf)):
         unpacked_g_rbuf[x] = (unpacked_g_rbuf[x]/1000)
-    testplot(np.arange (0, np.pi*2 , (1/512*np.pi*2)), unpacked_g_rbuf)
+    testplot(np.arange (0, 1000000, 1), unpacked_g_rbuf)
 
 if __name__ == "__main__":
-
     f = FPGA()
     if (False == f.init_device()):
         raise SystemExit
-
     #Wait for the configuration
     time.sleep(3)
     write_sin_wave(1)
-    f.xem.SetWireInValue(0x01, 0xF4240)
+    f.xem.SetWireInValue(0x01, 0xFFFF)
     f.xem.UpdateWireIns()
-    
