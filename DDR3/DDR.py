ea242059a3a3915a0b06d6f60720cd136a39bafe
NathanLoPresto <nlopresto14@gmail.com>
2021-07-15 14:00:36 -0500

    Changes made to the bit length

--- DDR3/DDR.py
+++ DDR3/DDR.py
@@ -22,11 +22,11 @@ FREQUENCY_PARAM = 10
 READBUF_SIZE = (8*1024*1024)
 
 #given the amplitude, and the time between each step, returns array to be plotted
-def make_step_function():
+def make_step_function(input_voltage):
     time_axis = np.arange (0, np.pi*2 , (1/1000000*2/np.pi) )
     amplitude = np.arange (0, np.pi*2 , (1/1000000*2/np.pi) )
     for x in range (len(amplitude)):
-        amplitude[x] =10000
+        amplitude[x] = input_voltage
     amplitude = amplitude.astype(np.int32)
     return time_axis, amplitude
 
@@ -118,9 +118,9 @@ def write_sin_wave (a):
     writeSDRAM(pass_buf)
 
 #given and amplitude and a period, it will write a step function to the DDR3 
-def write_step_func():
-    time_axis, g_buf_init = make_step_function()
-    testplot(time_axis, g_buf_init)
+def write_step_func(input_voltage):
+    time_axis, g_buf_init = make_step_function(input_voltage)
+    #testplot(time_axis, g_buf_init)
     pass_buf2 = bytearray(g_buf_init)
     writeSDRAM(pass_buf2)
 
@@ -141,12 +141,13 @@ if __name__ == "__main__":
     #Wait for the configuration
     time.sleep(3)
 
-    f.xem.SetWireInValue(0x04, 0x969925)
+    #f.xem.SetWireInValue(0x04, 0x969925)
+    f.xem.SetWireInValue(0x04, 0xFF)
     f.xem.UpdateWireIns()
 
     #Sample rate speed, to bits 18:9
     f.xem.SetWireInValue(0x02, 0x0000A000, 0x0003FF00 )
     f.xem.UpdateWireIns()
-    write_sin_wave(10)
+    write_step_func(8192)
 
 
