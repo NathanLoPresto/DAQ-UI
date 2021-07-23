3b6de7bb4b0611bfab4e6b43108db625c12bee8d
NathanLoPresto <nlopresto14@gmail.com>
2021-07-23 08:14:52 -0500

    1 hz clock bitfile

--- DDR3/DDR.py
+++ DDR3/DDR.py
@@ -4,12 +4,23 @@ import numpy as np
 import time
 import matplotlib.pyplot as plot
 import struct
+from collections import namedtuple
 
 BLOCK_SIZE = (16384)
 WRITE_SIZE=(8*1024*1024)
 READ_SIZE = (8*1024*1024)
 g_nMemSize = (8*1024*1024)
 sample_size = (524288)
+ep = namedtuple('ep', 'addr bits type')
+control =   ep(0x00, [i for i in range(32)], 'wi')  # note this is active low 
+
+# wire outs for "1 deep FIFO" 
+one_deep_fifo =   ep(0x20, [i for i in range(32)], 'wo')
+
+# triggers in 
+valid      = ep(0x40, 0, 'ti')
+fpga_reset = ep(0x40, 1, 'ti')
+fifo_reset = ep(0x40, 2, 'ti')
 
 #given the amplitude, and the time between each step, returns array to be plotted
 def make_flat_voltage(input_voltage):
@@ -40,14 +51,10 @@ def writeSDRAM(g_buf):
 
     print ("Length of buffer at the top of WriteSDRAM", len(g_buf))
     #Reset FIFOs
-    f.xem.SetWireInValue(0x03, 0x0004)
-    f.xem.UpdateWireIns()
-    f.xem.SetWireInValue(0x03, 0x0000)
-    f.xem.UpdateWireIns()
+    f.set_wire(0x30, 4)
+    f.set_wire(0x03, 0)
+    f.set_wire(0x03, 2)
 
-    #Enable SDRAM write memory transfers
-    f.xem.SetWireInValue(0x03, 0x0002)
-    f.xem.UpdateWireIns()
     print ("Writing to DDR...")
     time1 = time.time()
     #for i in range ((int)(len(g_buf)/WRITE_SIZE)):
@@ -61,14 +68,9 @@ def writeSDRAM(g_buf):
     print ("The speed of the write was ", mbs, " MegaBytes per second")
 
     #below sets the HDL into read mode
-    f.xem.UpdateWireOuts()
-    f.xem.SetWireInValue(0x03, 0x0004)
-    f.xem.UpdateWireIns()
-    f.xem.SetWireInValue(0x03, 0x0000)
-    f.xem.UpdateWireIns()
-    #Enable SDRAM write memory transfers
-    f.xem.SetWireInValue(0x03, 0x0001)
-    f.xem.UpdateWireIns()
+    f.set_wire(0x03, 4)
+    f.set_wire(0x03, 0)
+    f.set_wire(0x03, 1)
 
 #reads to an empty array passed to the function
 def readSDRAM():
@@ -76,15 +78,12 @@ def readSDRAM():
     pass_buf = bytearray(amplitude)
     #Reset FIFOs
     #below sets the HDL into read mode
-    f.xem.UpdateWireOuts()
-    f.xem.SetWireInValue(0x03, 0x0004)
-    f.xem.UpdateWireIns()
-    f.xem.SetWireInValue(0x03, 0x0000)
-    f.xem.UpdateWireIns()
-    #Enable SDRAM write memory transfers
-    f.xem.SetWireInValue(0x03, 0x0001)
-    f.xem.UpdateWireIns()
+    f.set_wire(0x03, 4)
+    f.set_wire(0x03, 0)
+    f.set_wire(0x03, 1)
+
     print ("Reading from DDR...")
+    #Address changed to A5
     for i in range ((int)(g_nMemSize/WRITE_SIZE)):
         r = f.xem.ReadFromBlockPipeOut( epAddr= 0xA0, blockSize= BLOCK_SIZE,
                                       data= pass_buf)
@@ -128,12 +127,18 @@ def print_DDR3():
     for x in range (len(unpacked_g_rbuf)):
         unpacked_g_rbuf[x] = (unpacked_g_rbuf[x]/1000)
     testplot(np.arange (0, sample_size, 1), unpacked_g_rbuf)
+def send_trig(ep_bit):
+    ''' 
+        expects a single bit, not yet implement for list of bits 
+    '''
+    f.xem.ActivateTriggerIn(ep_bit.addr, ep_bit.bits)
 
 if __name__ == "__main__":
 
-    f = FPGA(bitfile = 'OldVersion2.bit')
+    f = FPGA(bitfile = 'pipe1.bit')
     if (False == f.init_device()):
         raise SystemExit
+        
     #Wait for the configuration
     time.sleep(3)
     factor = (int)(sample_size/8)
@@ -144,7 +149,32 @@ if __name__ == "__main__":
     #Sample rate speed, to bits 18:9
     f.xem.SetWireInValue(0x02, 0x0000A000, 0x0003FF00 )
     f.xem.UpdateWireIns()
-    write_flat_voltage(10000)
+    write_flat_voltage(10922)
     time.sleep(2)
-    f.xem.UpdateWireOuts()
-    print (f.xem.GetWireOutValue(0x3E))
+    #f.xem.UpdateWireOuts()
+    #print (f.xem.GetWireOutValue(0x3E))
+    '''
+    time.sleep(2)
+    dacs = [1,2,3,4]
+    # SPI Master configuration: divide reg, ctrl reg, SS register 
+    # MSB: 8 - set address, 4 - write data  
+
+    # creg_val = 0x40003610 # Char length of 16; set both Tx_NEG, Rx_NEG; set ASS, IE. ADS7952
+    creg_val = 0x40003010 # Char length of 16; clear both Tx_NEG, Rx_NEG; set ASS, IE. AD5453    
+    # val = 0x40001fff # AD5453 (half-scale)
+
+    for val in [0x80000051, 0x40000013,  # divider (need to look into settings of 1 and 2 didn't show 16 clock cycles) 
+                0x80000041, creg_val,  # control register (CHAR_LEN = 16, bits 10,9, 13 and 12)
+                0x80000061, 0x40000001]: # slave select (just setting bit0)
+
+        f.set_wire(0x00, val, mask = 0xffffffff)
+        send_trig(valid) 
+
+    # now send SPI command 
+    value = 0x40003FFF # AD5453 (half-scale)
+
+    for val in [0x80000001, value,  # Tx register, data to send  
+                0x80000041, creg_val | (1 << 8)]: # Control register - GO (bit 8)
+        f.set_wire(0x00, val, mask = 0xffffffff)
+        send_trig(valid) 
+'''
