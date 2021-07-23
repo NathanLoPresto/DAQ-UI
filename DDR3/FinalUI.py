5424461db27493556e41741f192f36af13f43155
NathanLoPresto <nlopresto14@gmail.com>
2021-07-23 12:27:45 -0500

    Commented code

--- DDR3/FinalUI.py
+++ DDR3/FinalUI.py
@@ -1,67 +1,66 @@
-import numpy as np
-from fpga import FPGA
+from PyQt5 import QtWidgets, QtCore, QtGui
+from pyqtgraph import PlotWidget, plot
 from collections import namedtuple
+from PyQt5.QtWidgets import * 
+from PyQt5.QtCore import * 
+from PyQt5.QtGui import *
+from scipy import signal
+import pyqtgraph as pg
+from fpga import FPGA 
+import numpy as np
 import datetime
-import time
 import platform
-import ok
+import os.path
+import time
 import json
 import h5py
 import sys
-from PyQt5 import QtWidgets, QtCore, QtGui
-from pyqtgraph import PlotWidget, plot
-import pyqtgraph as pg
-from PyQt5.QtCore import * 
-from PyQt5.QtGui import * 
-from PyQt5.QtWidgets import * 
-from scipy import signal
-import os.path
+import ok
 
+#Input your values for adc address, if used, and downsample factor
+ep                = namedtuple('ep', 'number addr used downsample_factor')
+adc1              = ep(0, 0xA0, True,  1)
+adc2              = ep(1, 0xA1, True,  1)
+adc3              = ep(2, 0, False, 1)
+adc4              = ep(3, 0, False, 1)
+adc_list          = [adc1, adc2, adc3, adc4]
 
-#Input your values for adc address, trig address for each adc, and then then the mask
-ep = namedtuple('ep', 'number addr used downsample_factor')
+#These will eventually be taken from top-down file
+save_hdf5         = 'C:/Users/nalo1/Downloads/HDF5'
+save_json         = 'C:/Users/nalo1/Downloads/Metadata'
 
-adc1 = ep(0, 0xA0, True,  1)
-adc2 = ep(1, 0xA1, True,  1)
-adc3 = ep(2, 0, False, 1)
-adc4 = ep(3, 0, False, 1)
-adc_list = [adc1, adc2, adc3, adc4]
+#Data_Set should add more rows if more ADC inputs
+start_time        = time.time()
+now               = datetime.datetime.now()
+current_time      = now.strftime("%H_%M_%S")
+sample_size       = (524288)
+BLOCK_SIZE        = (16384)
+WRITE_SIZE        = (8*1024*1024)
+transfer_length   = (4096)
+g_nMemSize        = (8*1024*1024)
+pipe_addr_list    = [0x80, 0x80]
+data_set          = [[],[],[],[]]
+v_scaling         = 152.6e-6
 
-save_hdf5 = 'C:/Users/nalo1/Downloads/HDF5'
-save_json = 'C:/Users/nalo1/Downloads/Metadata'
-
-start_time= time.time()
-now = datetime.datetime.now()
-current_time = now.strftime("%H_%M_%S")
-sample_size = (524288)
-BLOCK_SIZE = (16384)
-WRITE_SIZE=(8*1024*1024)
-transfer_length=(4096)
-g_nMemSize = (8*1024*1024)
-pipe_addr_list = [0x80, 0x80]
-data_set = [[],[],[],[]]
-
-#Creates a file with a single dataset, named with time of day
+#Creates both an HDF5 file with data, and JSON with metadata
 def filemaker():
     nom = os.path.join(save_hdf5,"OPAMPDATA" + (str)(current_time)+ ".hdf5")
-    hf = h5py.File(nom, 'w')
+    hf  = h5py.File(nom, 'w')
     for x in adc_list:
         hf.create_dataset((str)((str)(x)+ "dataset"), data=data_set[x.number])
-
     hf.close()
 
-    data=get_meta_data()
+    data = get_meta_data()
     nom2 = os.path.join(save_json,'metadata' + (str) (current_time) + ".json")
     with open (nom2, 'w') as outfile:
         json.dump(data, outfile)
 
+#Used as a twos_comp converter for the convert_data function
 def twos_comp(val, bits):
-
     def twos_comp_scalar(val, bits):
         if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
             val = val - (1 << bits)        # compute negative value
         return val                         # return positive value as is
-
     if hasattr(val, "__len__"):
         tmp_arr = np.array([])
         for v in val:
@@ -70,8 +69,8 @@ def twos_comp(val, bits):
     else:
         return twos_comp_scalar(val, bits)
 
+#Given a buffer from the read_pipe, converts into float-type for graphing
 def convert_data(buf):
-
     bits = 16 # for AD7961 
     # bits=18 for AD7960
     d = np.frombuffer(buf, dtype=np.uint8).astype(np.uint32)
@@ -81,30 +80,31 @@ def convert_data(buf):
         # TODO: test and verify  
         d2 = (d[3::4]<<8) + d[1::4] + ((d[0::4]<<16) & 0x03)
         # d2 = (d[3::4]<<8) + d[1::4] + ((d[0::4]<<16) & 0x03)
-
     d_twos = twos_comp(d2, bits)
     return d_twos
 
+#Read from pipe-out, returns a float-array for graphing
 def adc_return(fpga, adc_chan = 0, filename = None, PLT = False):
-    s,e = fpga.read_pipe_out(addr_offset = adc_chan, data_len=transfer_length)
-    d = convert_data(s)
-    v_scaling = 152.6e-6
-    r=(d*v_scaling)
+    s,e       = fpga.read_pipe_out(addr_offset = adc_chan, data_len=transfer_length)
+    d         = convert_data(s)
+    r         = (d*v_scaling)
     return r
 
+#returns a dictionary of metadata for JSON file creation
 def get_meta_data():
     meta_dict = {
-        "Time" : (str)(current_time),
-        "Date" : "{:%d, %b %Y}".format(datetime.date.today()),
-        "Firmware version": f.xem.GetDeviceMajorVersion(),
-        "Product" : f.xem.GetBoardModel(),
+        "Time"                  : (str)(current_time),
+        "Date"                  : "{:%d, %b %Y}".format(datetime.date.today()),
+        "Firmware version"      : f.xem.GetDeviceMajorVersion(),
+        "Product"               : f.xem.GetBoardModel(),
         "Product Serial Number" : f.xem.GetSerialNumber(),
-        "Device ID" : f.xem.GetDeviceID(),
-        "OS" : platform.system(),
-        "OS Version" : platform.version()
+        "Device ID"             : f.xem.GetDeviceID(),
+        "OS"                    : platform.system(),
+        "OS Version"            : platform.version()
     }
     return meta_dict
 
+#Called by Control-C During graphing to save data and exit the program
 def save_and_exit():
   print ("Saving and exiting the program")
   if (len(data_set)!=0):
@@ -117,15 +117,16 @@ def make_sin_wave(amplitude_shift):
     amplitude = (amplitude_shift*1000*np.sin(time_axis))
     y = len(amplitude)
     for x in range (y):
-        amplitude[x]= amplitude[x]+(10000)
+        amplitude[x] = amplitude[x]+(10000)
     for x in range (y):
-        amplitude[x]= (int)(amplitude[x]/20000*16384)
+        amplitude[x] = (int)(amplitude[x]/20000*16384)
     for x in range(y):
         amplitude[x] = amplitude[x]+5000
     amplitude = amplitude.astype(np.int32)
     byteamp = bytearray(amplitude)
     return byteamp
 
+#Given a single 14-bit value, writes full data set at that DAC value
 def make_flat_voltage(input_voltage):
     time_axis = np.arange (0, np.pi*2 , (1/sample_size*2*np.pi) )
     amplitude = np.arange (0, np.pi*2 , (1/sample_size*2*np.pi) )
@@ -135,8 +136,8 @@ def make_flat_voltage(input_voltage):
     byteamp = bytearray(amplitude)
     return byteamp
 
+#Given a buffer and DDR address, writes to SDRAM
 def writeSDRAM(g_buf, address):
-
     #Reset FIFOs
     f.set_wire(0x30, 4)
     f.set_wire(0x03, 0)
@@ -145,11 +146,13 @@ def writeSDRAM(g_buf, address):
     r = f.xem.WriteToBlockPipeIn( epAddr= address, blockSize= BLOCK_SIZE,
                                       data= g_buf[0:(len(g_buf))])
     print ("Written length was: ", r)
+
     #below sets the HDL into read mode
     f.set_wire(0x03, 4)
     f.set_wire(0x03, 0)
     f.set_wire(0x03, 1)
 
+#Creates 4 graphing windows if enabled, sends pinter to ADC namedtuple
 def main_loop():
     if (f.xem.NoError != f.xem.OpenBySerial("")):
             print ("You can't run the software if no device is detected")
@@ -167,7 +170,7 @@ def main_loop():
             
         sys.exit(app.exec_())
 
-#Qt5 window class, to be initializaed upon call to Main.py
+#Qt5 window class, to be initializaed upon call to main loop
 class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self,chan, *args, **kwargs):
@@ -203,6 +206,8 @@ class MainWindow(QtWidgets.QMainWindow):
         except KeyboardInterrupt:
             save_and_exit()
 
+#Initialized FPGA and runs main loop
+#TODO: Check PLL reset from UI.py
 if __name__ == "__main__":
 
     f = FPGA(bitfile = 'pipe1.bit')
